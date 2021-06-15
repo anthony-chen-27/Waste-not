63ef81e15a2a2c1563c4ddd51a9f5c3fa166907e
simpleistruth <45241193+simpleistruth@users.noreply.github.com>
2021-06-15 16:59:21 -0500

    removed login and signup routes since we have modals now, changed so that protected routes redirect home instead of /login

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -3,8 +3,6 @@ import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 import MainPage from "./main/main_page";
-import LoginFormContainer from "./session/login_form_container";
-import SignupFormContainer from "./session/signup_form_container";
 import ProfileContainer from "./profile/profile_container";
 import RestaurantCompose from "./restaurant/restaurant_compose";
 import Restaurant from "./restaurant/restaurant";
@@ -17,8 +15,6 @@ const App = () => (
     <NavBarContainer />
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
-      <AuthRoute exact path="/login" component={LoginFormContainer} />
-      <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
       <ProtectedRoute exact path="/restaurants" component={Restaurant} />
       <ProtectedRoute exact path="/profile" component={ProfileContainer} />
