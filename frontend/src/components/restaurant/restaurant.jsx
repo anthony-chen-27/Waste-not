a26860fceb57c337b6fab1a5b3c4564c2b66cc24
Kaz DeBear <kazdb@me.com>
2021-06-15 11:50:39 -0700

    Basic styling for restaurant page

--- frontend/src/components/restaurant/restaurant.jsx
+++ frontend/src/components/restaurant/restaurant.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+    width: 98.5vw;
+    height: 88vh;
+    border: 1px solid green;
+    display: flex;
+`;
+
+const RestaurantWrapper = styled.div`
+    width: 50%;
+    border: 1px solid blue;
+`;
+
+const RestaurantFilterWrapper = styled.div`
+    width: 100%;
+    height: 15%;
+    border: 1px solid red;
+`;
+
+const RestaurantCardsWrapper = styled.div`
+    width: 100%;
+    height: 85%;
+    border: 1px solid orange;
+`;
+
+const MapWrapper = styled.div`
+    width: 50%;
+    height: 100%
+    border: 1px solid grey;
+`;
 
 const Restaurant = () => {
-    const Rapper = styled.div``
     return (
-        <Rapper>
-            Main splash page
-        </Rapper>
+        <Wrapper>
+            <RestaurantWrapper>
+                <RestaurantFilterWrapper>
+                    Filter
+                </RestaurantFilterWrapper>
+                <RestaurantCardsWrapper>
+                    Restaurant Card
+                </RestaurantCardsWrapper>
+            </RestaurantWrapper>
+            <MapWrapper>
+                Map
+            </MapWrapper>
+        </Wrapper>
     )
 }
 
