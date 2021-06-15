a3be838ab05c40221b6e815bd87758dbe0f90771
Anthony <chen.j.anthony@gmail.com>
2021-06-15 12:19:51 -0500

    deleted all tweet files and created empty restaurant components

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
-
-import TweetsContainer from './tweets/tweets_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
-import RestaurantCompose from './restuarant/restaurant_compose';
+import RestaurantCompose from './restaurant/restaurant_compose'
 import Restaurant from './restaurant/restaurant';
 
 const App = () => (
@@ -19,7 +17,7 @@ const App = () => (
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
-      <ProtectedRoute exact path="/restaurants" component={Restaurants} />
+      <ProtectedRoute exact path="/restaurants" component={Restaurant} />
       <ProtectedRoute exact path="/profile" component={ProfileContainer} />
       <ProtectedRoute exact path="/new_restaurant" component={RestaurantCompose} />
     </Switch>
